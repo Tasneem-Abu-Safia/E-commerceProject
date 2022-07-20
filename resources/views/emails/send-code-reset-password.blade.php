32ac8aa6e280cf15b94d9d95ee2d61caf91684eb
Tasneem safia <76720686+Tasneem-Abu-Safia@users.noreply.github.com>
2022-07-20 18:50:01 +0300

    emailVerification commit

--- resources/views/emails/send-code-reset-password.blade.php
+++ resources/views/emails/send-code-reset-password.blade.php
@@ -1,10 +1,8 @@
 @component('mail::message')
-    <h1>We have received your request to reset your account password</h1>
-    <p>You can use the following code to recover your account:</p>
-
+    We have received your request to reset your account password
+    You can use the following code to recover your account:
     @component('mail::panel')
         {{ $code }}
     @endcomponent
-
-    <p>The allowed duration of the code is one hour from the time the message was sent</p>
-@endcomponent
+    The allowed duration of the code is one hour from the time the message was sent
+    @endcomponent
